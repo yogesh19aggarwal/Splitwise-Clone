4046300c7bffb45837bd0b794c498eb7d115f3f9
yogesh19aggarwal <yogesh.19.aggarwal@gmail.com>
2025-03-20 14:08:42 +0530

    Add tailwind css

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import { registerRootComponent } from 'expo';
+import "./global.css";
+import { registerRootComponent } from "expo";
+import { ExpoRoot } from "expo-router";
 
-import App from './App';
+export function App() {
+  const ctx = require.context("./app");
+  return <ExpoRoot context={ctx} />;
+}
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
 registerRootComponent(App);
